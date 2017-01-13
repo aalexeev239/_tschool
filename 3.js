b25676c9058945ea8a52659104bd9f032136cc33
a.alekseev10 <a.alekseev10@tinkoff.ru>
2017-01-13 18:18:15 +0300

    up

--- 3.js
+++ 3.js
@@ -1,26 +1,117 @@
-var owl = {
-    name: 'Сова',
-    says: function () {
-        console.log(this.name + ' говорит: ух!');
+// var owl = {
+//     name: 'Сова',
+//     says: function () {
+//         console.log(this.name + ' говорит: ух!');
+//     }
+// };
+//
+// var bigOwl = {
+//     name: 'Большая сова',
+//     says: function () {
+//         console.log(this.name + ' говорит: ух!');
+//     }
+// };
+//
+// owl.says === bigOwl.says; // false
+
+// function Owl(name) {
+//     this.name = name;
+// }
+//
+// Owl.prototype.says = function () {
+//     console.log(this.name + ' говорит: ух!');
+// };
+//
+// var owl = new Owl('Сова');
+// var bigOwl = new Owl('Большая сова');
+//
+// bigOwl.says = function () {
+//     console.log(this.name + ' говорит: УХ!');
+// };
+//
+// owl.says(); // Сова говорит: ух!
+// bigOwl.says(); // Большая сова говорит: УХ!
+
+
+// function Owl(name) {
+//     this.name = name;
+// }
+//
+// Owl.prototype.says = function () {
+//     console.log(this.name + ' говорит: ух!');
+// };
+//
+// Owl.prototype.toString = function () {
+//     console.log('cова по имени ' + this.name);
+// };
+//
+// var owl = new Owl('Сова');
+//
+// console.log(owl.toString()); // cова по имени Сова
+
+// // console.dir(owl);
+// console.log(owl.toString()); // [object Object]
+// console.log({}.toString()); // [object Object]
+//
+// function Animal(name) {
+//     this.name = name;
+// }
+//
+// Animal.prototype.sleep = function () {
+//     console.log(this.name + ' спит');
+// };
+//
+// Animal.prototype.hunt = function () {
+//     console.log(this.name + ' охотится');
+// };
+
+//
+//
+// function Owl(name) {
+//     Animal.call(this, name);
+// }
+
+// var emptyConstructor = function () {};
+// emptyConstructor.prototype = Animal.prototype;
+// Owl.prototype = new emptyConstructor();
+// Owl.prototype.says = function () {
+//     console.log(this.name + ' говорит: ух!');
+// };
+
+// Owl.prototype = Object.create(Animal.prototype);
+// Owl.prototype.constructor = Owl;
+
+
+//
+//
+// var owl = new Owl('Сова');
+// owl.sleep();
+// owl.hunt();
+// owl.says();
+
+class Animal {
+    constructor(name) {
+        this.name = name;
     }
-};
 
-
-var owl2 = {
-    name: 'Ещё одна сова',
-    says: function () {
-        console.log(this.name + ' говорит: ух!');
+    sleep() {
+        console.log(this.name + ' спит');
     }
-};
 
-function Owl(name) {
-    this.name = name;
+    hunt() {
+        console.log(this.name + ' охотится');
+    }
 }
 
-Owl.prototype.says = function () {
-    console.log(this.name + ' говорит: ух!');
-};
+class Owl extends Animal {
+    constructor(name) {
+        super(name);
+    }
 
-var owl3 = new Owl('Прототипная сова');
-ow3.says();
+    says() {
+        console.log(this.name + ' говорит: ух!');
+    }
+}
+
+var owl = new Owl('Сова');
 
